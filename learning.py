26449b99ade9281276565469c39a4a10e382d519
Leïza Bobegoto <fatimabobegoto@gmail.com>
2026-09-25 14:58:12 +0200

    First commit

---
+++ learning.py
@@ -0,0 +1,14 @@
+age = 20
+name = "Leiza"
+//exemple of template literals named f-string
+print(f"My name is {name} and I am {age}");
+
+//exo 1
+age1 = 20
+if age < 13 :
+    print("Child")
+    else if age >= 13 and age < 17:
+        print("teenager")
+        else if age >= 18:
+            print("Adult")
+            
